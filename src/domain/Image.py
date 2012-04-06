0146599634cd44d191e9c993d9ce07c33512a882
unknown <Jeferson@Master.(none)>
2012-04-06 19:47:51 -0300

    project startup

---
+++ src/domain/Image.py
@@ -0,0 +1,26 @@
+'''
+Created on 06/04/2012
+
+@author: Jeferson
+'''
+
+from array import array
+
+class Image(object):
+
+    def __init__(self, width, height):
+        self.width  = width
+        self.height = height
+        self.pixels = array("i", xrange(self.width*self.height))
+
+    def setPixel(self, rgbColor, x, y):
+        self.pixels[x+y*self.width] = rgbColor
+    
+    def getPixel(self, x, y):
+        return self.pixels[x+y*self.width]
+    
+    def getWidth(self):
+        return self.width
+    
+    def getHeight(self):
+        return self.height
---
+++ src/engine/JoinerEngine.py
@@ -0,0 +1,10 @@
+'''
+Created on 06/04/2012
+
+@author: Jeferson
+'''
+
+class JoinerEngine(object):
+    
+    def __init__(self, *args, **kwargs):
+        object.__init__(self, *args, **kwargs)
